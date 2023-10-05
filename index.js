f1b94b8d22568401556bbf93cc2635111d9ec4f9
Okononfua Mathew <oseghaleokononfua@gmail.com>
2023-10-05 11:25:13 +0100

    LEARNING TYPESCRIPT

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+let id = 5;
+let company = 'MathewPio';
+let isPublished = true;
+let x = 'Hello';
+x = true;
+let ids = [1, 2, 3, 4, 5];
+// Writing functions 
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(1, 3));
